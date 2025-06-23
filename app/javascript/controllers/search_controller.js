e0346d9365c0459bd0d7415798435e23f274bbe5
dan <dis.131998@gmail.com>
2025-06-23 13:11:37 +0300

    add: functionality to close the search block when clicking outside element

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,6 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+   let body = document.body;
+   let turboResults = document.querySelector(".b-search");
+   body.addEventListener("click", (event) => {
+
+      if (!turboResults.contains(event.target)) {
+      turboResults.parentElement.removeAttribute("src")
+      turboResults.remove()
+      }
+    })
+  }
 
   hide() {
     this.element.parentElement.removeAttribute("src")
