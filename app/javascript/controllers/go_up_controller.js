4972880d79c8a43417ba2bd422f95769db2e7b4f
dan <dis.131998@gmail.com>
2025-06-26 17:00:26 +0300

    add: go to page top link stimulus controller

---
+++ app/javascript/controllers/go_up_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    let mylink = document.getElementById("top-link");
+    mylink.style.display = "none";
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.documentElement.scrollTop > 20) {
+      mylink.style.display = "block";
+    } else {
+      mylink.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  mylink.addEventListener("click", topFunction)
+  function topFunction() {
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+  }
+
+}
