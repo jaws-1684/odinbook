18e8db9c1b673f3f077ed95738e3d9f0d881e306
dan <dis.131998@gmail.com>
2025-07-18 18:07:34 +0300

    fix: devise mailer_sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'https://odinbook@sendgrid.com'
+  config.mailer_sender = 'odinbook@sendgrid.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
