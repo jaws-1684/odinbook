1b242368a4c0c41681608065361af688355e5d94
dan <dis.131998@gmail.com>
2025-07-19 13:07:16 +0300

    fix: stimulus copy controller

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -4,12 +4,13 @@ export default class extends Controller {
   
   copy(event) {
     event.preventDefault()
-    const container = event.target.parentElement;
-    let data = container.querySelector('#value')
-  
-    
-     navigator.clipboard.writeText(data.textContent);
+    const para = document.querySelector('#copy-result')
+    const elementcontainer = event.target.parentElement;
 
+    let data = elementcontainer.querySelector('#value') 
+    navigator.clipboard.writeText(data.textContent);
+
+    para.textContent = "Copied"
     }
   
 }
